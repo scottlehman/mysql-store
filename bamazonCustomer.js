757b97fd6b7451d51a546f92c2fe6850cb14d16f
Scott Lehman <scottsgllehman@gmail.com>
2019-03-17 22:13:53 -0600

    no pass

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -6,7 +6,7 @@ var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "27.77.26.207",
+    password: "",
     database: "bamazon"
 });
 
