de5ea1b83aec1b09cd2fecf3310cad65f1d32a98
Scott Lehman <scottsgllehman@gmail.com>
2019-03-21 18:21:38 -0600

    updated readme

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # mysql-store
-Amazon like database with updates
+Amazon like database with updates to the MySQL table. 
+
+Once you initial the app in your terminal it will present you with a table of the items in stock and shows 
+price and quantity available.
+
+
+Once you have entered in the ID number of the item you would like to purchase it will ask how many would like to order next.
+
+Once your order is fulfilled the the MySQL table is updated. I console logged to event to show the update.
+
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,56 +1,121 @@
 var mysql = require('mysql');
 var inquirer = require('inquirer');
+var Table = require("cli-table3");
 
 var connection = mysql.createConnection({
 
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "",
+    password: "27.77.26.207",
     database: "bamazon"
 });
 
 connection.connect(function (err) {
     if (err) throw err;
 
-    displayProducts();
+    //Calling for Table to be displayed in CLI terminal
+    InventoryTable();
 });
 
-function displayProducts() {
-    connection.query("SELECT * FROM products", function (err, results) {
-        if (err) throw err;
-        purchaseQuery();
-    })
-};
+function InventoryTable() {
 
-function purchaseQuery() {
-     
-        inquirer.prompt([
-            {
-                name: "products",
-                type: "rawlist",
-                choices: function() {
-                    var productsArray = [];
+    //Defining Table with Headers for relevant table information
+    var table = new Table({
+        head: ['ID', "Product", "Price", "Stock"],
+        colWidths: [10, 30, 30, 30]
+    });
 
-                    for(var i = 0; i < results.length; i++) {
-                        productsArray.push(results[1]);
+    //calling Inventory function below
+    inventory();
 
-                    }
-                    return productsArray;
+    //Pulling MySQL table rows and setting them to vars for pushing to CLI table
+    function inventory() {
 
-                },
-                message: "Which item would like to purchase today?"
-            },
-            {
-            name: "order",
-            type: "input",
-            message: "How many would you like to on your order?",
+        connection.query("SELECT * FROM products", function (err, results) {
+
+            for (var i = 0; i < results.length; i++) {
+                // console.log(results[i]);
+                var itemId = results[i].item_id;
+                var product = results[i].product_name;
+                var price = results[i].price;
+                var stock = results[i].stock_quantity;
+
+                // console.log(itemId);
+                // console.log(product);
+                // console.log(price);
+                // console.log(stock);
+
+                //Pushing infor to table for display
+                table.push(
+                    [itemId, product, price, stock]
+                );
             }
-        ])
-        .then(function(answer) {
-            var item;
-            var quantity = 0;
 
+            //Displaying table in terminal
+            console.log(table.toString());
+            purchaseItem();
+        });
+    }
+}
+
+function purchaseItem() {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "itemId",
+            message: "Enter the item ID of the product you would like to purchase."
+        },
+        {
+            type: "input",
+            name: "quantity",
+            message: "Enter the quantity for the item you wish to purchase.",
+            // validate: function validateNumb (answer) {
+            //     if (isNaN(answer)) {
+            //         console.log("Please enter a number.")
+            //         return false;
+            //     }
+            // }
+        }
+    ])
+    .then(function(answer) {
+
+        connection.query("SELECT * FROM products WHERE item_id=?", answer.itemId, function(err, results) {
+            for (var i = 0; i < results.length; i++) {
+                if (answer.quantity > results[i].stock_quantity) {
+
+                    console.log("Unable to complete request: NOT ENOUGH STOCK, please try again.");
+                    purchaseItem();
+                }else {
+                    console.log(`Your order has been fulfilled.\nYour order:\n---------------------\nItem: ${results[i].product_name}\nQuantity: ${answer.quantity}`)
+                    var newStock = (results[i].stock_quantity - answer.quantity);
+                    var purchasedProduct = answer.itemId;
+                }
+            }
+
+            
+
+            updateProduct(newStock, purchasedProduct);
         })
     })
+
 }
+
+
+function updateProduct(newStock, purchasedProduct) {  
+    var query = connection.query("UPDATE products SET ? WHERE ?", 
+    [
+        {
+          stock_quantity: newStock
+        },
+  
+        {
+          item_id: purchasedProduct
+        }
+      ],
+  
+    );
+
+    // console.log(query.sql);
+  }
+  
